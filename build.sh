e0cae2b3343cc765320757f641e056ee067424f0
Alexander Vetter <vettera@gmail.com>
2016-01-07 19:25:30 +0100

    Switch to latexmk some other build stuff

--- build.sh
+++ build.sh
@@ -17,21 +17,4 @@ fi
 export LC_CTYPE=de_DE.UTF-8
 export LC_ALL=de_DE.UTF-8
 
-function call_pdflatex {
-  pdflatex -interaction=batchmode Masterdatei 1>/dev/null
-}
-
-echo "#1 pdflatex"
-call_pdflatex
-
-echo "#2 makeglossaries"
-makeglossaries -q Masterdatei
-
-echo "#3 bibtex"
-bibtex -terse Masterdatei
-
-echo "#4 pdflatex"
-call_pdflatex
-
-echo "#5 pdflatex"
-call_pdflatex
+latexmk -pdf Masterdatei
